99c380af1ea6935db04f891206757fadc5f9a04d
newming <a977527479@sina.com>
2017-07-13 14:33:53 +0800

    change hashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 import {connect} from 'react-redux';
 import Header from './components/Header'
 import Footer from './components/Footer'
